5565c0d13dd0bac2adfe36c019738584a9263840
ASFahim007 <devasfahim@gmail.com>
2022-09-03 15:47:40 +0600

    category API loded

--- js/app.js
+++ js/app.js
@@ -5,12 +5,30 @@ const loadCategory = async() => {
     const data = await res.json()
     displayCategory(data.data.news_category);
 }
-//category.category_name
+// display categories on category section
 const displayCategory = (categories) => {
     const categoryContainer = document.getElementById('category_container')
     categories.map(category => {
-        
+        const li = document.createElement('li');
+        li.innerHTML = `
+        <a href="">${category.category_name}</a>
+        `;
+        categoryContainer.appendChild(li)
     })
 }
-
 loadCategory()
+
+// const loadNews = async() => {
+//     const url = `https://openapi.programming-hero.com/api/news/category/01`
+//     const res = await fetch(url)
+//     const data = await res.json()
+//     displayNews(data.data);
+// }
+// const displayNews = (allNews) => {
+//     const 
+//     allNews.map(news => {
+//         console.log(news);
+
+//     })
+// }
+loadNews()
